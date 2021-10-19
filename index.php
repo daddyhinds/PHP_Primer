436545f01268ff555f0bc5ed5834e1a76d865dc6
daddyhinds <khinds20j@vtdi.edu.jm>
2021-10-18 22:32:54 -0500

    Update index.php
    Removed a button

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ echo '<h1>My Age Is: '.$age.'</h1>';
 echo "<h1>My Name is: $name </h1>";
 
 echo '<button type="button" class="btn btn-success">ClicK Me!</button>';
-echo '<button type="button" class="btn btn-dark">ClicK Me!</button>';
+//echo '<button type="button" class="btn btn-dark">ClicK Me!</button>';
 echo '<button type="button" class="btn btn-primary">ClicK Me!</button>';
 
 ?>
